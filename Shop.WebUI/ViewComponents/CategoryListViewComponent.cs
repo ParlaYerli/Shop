0383839eb94d8efd0abcbde866754218618fd1f4
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2021-01-05 16:16:01 +0300

    categorylistview component

---
+++ Shop.WebUI/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Business.Abstract;
+using Shop.WebUI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.WebUI.ViewComponents
+{
+    public class CategoryListViewComponent : ViewComponent
+    {
+        private ICategoryService _categoryService;
+        public CategoryListViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(new CategoryListViewModel()
+            {
+                Categories = _categoryService.GetAll()
+            });
+        }
+    }
+}
